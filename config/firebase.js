66c33f02e01144fef92ac9df10f6c38462f178e5
Chaimale Iken <c.iken@aui.ma>
2024-05-28 16:48:51 +0100

    Your commit message here

---
+++ config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+
+//Firebase config
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth= getAuth();
+export const database= getFirestore();
